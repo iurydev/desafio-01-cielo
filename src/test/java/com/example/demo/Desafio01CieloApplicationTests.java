34c46fa74a0a09d0aaeac0d1629a011ce4a10f1e
iurycastro <iurydev@gmail.com>
2023-09-26 21:40:49 -0300

    Commit inicial: Arquivos padrão do projeto.

---
+++ src/test/java/com/example/demo/Desafio01CieloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio01CieloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
