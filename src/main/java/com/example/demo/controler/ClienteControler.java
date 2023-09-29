f3524dc07ed54890552400b08ad5bc7b98731690
iurycastro <iurydev@gmail.com>
2023-09-29 17:06:35 -0300

    TASK: Migrando método de consulta e exclusão para a classe ClienteController

--- src/main/java/com/example/demo/controler/ClienteControler.java
+++ src/main/java/com/example/demo/controler/ClienteControler.java
@@ -3,6 +3,7 @@ package com.example.demo.controler;
 import com.example.demo.model.Cliente;
 import com.example.demo.model.PessoaFisica;
 import com.example.demo.model.PessoaJuridica;
+import com.example.demo.repository.ClienteRepository;
 import com.example.demo.repository.PessoaFisicaRepository;
 import com.example.demo.repository.PessoaJuridicaRepository;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cliente")
@@ -18,19 +20,40 @@ import java.util.List;
 public class ClienteControler {
     private final PessoaFisicaRepository pessoaFisicaRepository;
     private final PessoaJuridicaRepository pessoaJuridicaRepository;
+    private final        ClienteRepository clienteRepository ;
 
-    public ClienteControler(PessoaJuridicaRepository pessoaJuridicaRepository, PessoaFisicaRepository pessoaFisicaRepository) {
+    public ClienteControler(PessoaJuridicaRepository pessoaJuridicaRepository, PessoaFisicaRepository pessoaFisicaRepository, ClienteRepository clienteRepository) {
         this.pessoaFisicaRepository = pessoaFisicaRepository;
         this.pessoaJuridicaRepository = pessoaJuridicaRepository;
+        this.clienteRepository = clienteRepository;
     }
 
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Optional<Cliente>> consultar(@PathVariable Long id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        if (cliente.isEmpty()) {
+            return new ResponseEntity("Cliente não encontrado!", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Optional<Cliente>>(cliente, HttpStatus.OK);
+
+    }
     @GetMapping
-    public ResponseEntity<List<Cliente>> getAll() {
+    public ResponseEntity<List<Cliente>> consultarTodos() {
         List<Cliente> clientes = new ArrayList<>();
         List<PessoaFisica> ClientesPf = pessoaFisicaRepository.findAll();
         List<PessoaJuridica> ClientesPj = pessoaJuridicaRepository.findAll();
         clientes.addAll(ClientesPf);
         clientes.addAll(ClientesPj);
         return new ResponseEntity<>(clientes,HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> excluir(@PathVariable Long id) {
+        Optional<Cliente> cliente = clienteRepository.findById(id);
+        if (cliente.isEmpty()) {
+            return new ResponseEntity<>("Cliente não encontrado!", HttpStatus.NOT_FOUND);
+        }
+        clienteRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
