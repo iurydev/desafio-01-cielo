bb05b7ad81250e7dd0c7943c9797cba72b5542dd
iurycastro <iurydev@gmail.com>
2023-09-28 22:05:52 -0300

    TASK: Adionando repositories.

---
+++ src/main/java/com/example/demo/repository/PessoaJuridicaRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.PessoaJuridica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PessoaJuridicaRepository extends JpaRepository<PessoaJuridica, Long> {
+    Optional<PessoaJuridica> findByCnpj(String cnpj);
+}
