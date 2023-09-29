2bb0ee69506522188068d97e18a356aeb10d8bdf
iurycastro <iurydev@gmail.com>
2023-09-28 22:07:14 -0300

    TASK: Tratando as exceções de validação.

---
+++ src/main/java/com/example/demo/util/ValidacaoCustomizadaExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.demo.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ValidacaoCustomizadaExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldError().getDefaultMessage();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
+}
