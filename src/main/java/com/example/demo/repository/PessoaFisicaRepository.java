812a463e5c0c4059307ec3eff62a909064a2b8d7
iurycastro <iurydev@gmail.com>
2023-09-29 17:07:38 -0300

    TASK: Recuperação via CPF e CNPJ

--- src/main/java/com/example/demo/repository/PessoaFisicaRepository.java
+++ src/main/java/com/example/demo/repository/PessoaFisicaRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.PessoaFisica;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface PessoaFisicaRepository extends JpaRepository<PessoaFisica, Long> {
+    Optional<PessoaFisica> findByCpf(String cpf);
 }
--- src/main/java/com/example/demo/repository/PessoaJuridicaRepository.java
+++ src/main/java/com/example/demo/repository/PessoaJuridicaRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.model.PessoaJuridica;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface PessoaJuridicaRepository extends JpaRepository<PessoaJuridica, Long> {
