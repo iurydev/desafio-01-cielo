cbecd584244d12f323190a6b84199deb0deee4a3
iurycastro <iurydev@gmail.com>
2023-09-28 22:09:03 -0300

    TASK: Novas notacões do JPA e Mvc.

--- src/main/java/com/example/demo/Desafio01CieloApplication.java
+++ src/main/java/com/example/demo/Desafio01CieloApplication.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableWebMvc
 public class Desafio01CieloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio01CieloApplication.class, args);
+		System.out.println("Running App");
 	}
 
 }
